96eaeaa8fe26b4d8bdf8c780138be6f02c66e2e8
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2022-06-22 22:23:02 +0500

    .

--- wallet_saving_goals/lib/screen/home/kamittee_holder/holder_dashboard.dart
+++ wallet_saving_goals/lib/screen/home/kamittee_holder/holder_dashboard.dart
@@ -47,6 +47,51 @@ class HolderDashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Referal Code',
+                      isDense: true,
+                      filled: true,
+                      fillColor: AppColor.secondary.withOpacity(0.25),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintStyle: TextStyle(
+                        color: AppColor.fonts.withOpacity(0.5),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Join'),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(AppColor.white),
+                    overlayColor: MaterialStateProperty.all<Color>(
+                      AppColor.white.withOpacity(0.1),
+                    ),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(70, 45),
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
             child: Text(
               'Invitations',
               style: TextStyle(
